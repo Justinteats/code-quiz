7083d8cbd462119e38f9715f4da5a24f94b77a4c
Justin Teats <justinteats@Justins-MacBook-Air.local>
2022-07-04 17:37:39 -0700

    added functions and variables to js

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # code-quiz
 
-a short quiz on simple javascript questions
+a short quiz on javascript questions with multiple choice options.
+
 made for boot camp students by a bootcamp student.
+
+Click start quiz to begin 
+Time given is 60 seconds
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,9 @@ var nextButton = document.getElementById('next-btn')
 var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-buttons')
-var count = 30;
+var count = 60;
 var counter = 15
-var countdown =function () {
-    console.log(counter);
-    counter--;
-    if(counter === 0){
-        clearInterval(startCountdown);
-    };
-};
+
   var startCountdown = setInterval(countdown, 15000);
 
   //questions with choices and answers pulled from "https://www.includehelp.com/mcq/javascript-multiple-choice-questions-mcqs.aspx"
@@ -71,15 +65,82 @@ var countdown =function () {
     currentQuestionIndex++
     setNextQuestion()
   })
-  
-  function startTimer() {
+function startTimer() 
     var interval = setInterval(function() {
-      document.getElementById('time-display').innerHTML=count;
-      count--;
-      if (count < 0){
-        clearInterval(interval);
-        alert("You're out of time!");
+        document.getElementById('time-display').innerHTML=count;
+        count--;
+        if (count < 0){
+            clearInterval(interval);
+            alert('no more time left');
       }
-    },1000);
+    })
+
+function startGame() {
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+  }
+  
+  function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+  }
+  
+  function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+      var button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+   
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
+  }
+  
+  function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+  }
+  
+  function selectAnswer(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+
+    if(!correct){
+      count -= 5
+      document.getElementById('time-display').innerHTML=count;
+    }
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide')
+    } else {
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+    }
+  }
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
   }
   
